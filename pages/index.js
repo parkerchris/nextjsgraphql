40d17f7c05bd3cb3e7516cbe4e46eee6409e5760
Chris Parker <cwbparker@gmail.com>
2022-07-02 12:18:02 -0700

    Project setup and GraphCMS connected

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,64 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { GraphQLClient, gql } from 'graphql-request' 
 
-export default function Home() {
+/* const graphcms = new GraphQLClient('https://api-us-west-2.graphcms.com/v2/cl51ze6181wt601ukbse59thb/master')
+
+const QUERY = gql`
+{
+  properties {
+    street_address,
+    city,
+    state,
+    zip_code
+  }
+}
+`
+
+export async function getStaticProps() {
+  const {properties} = await graphcms.request(QUERY)
+  return{
+    props: {
+      properties,
+    },
+    revalidate: 10,
+  }
+} */
+
+
+
+export async function getStaticProps() {
+  const url = process.env.GRAPH_CMS_ENDPOINT
+  const Client = new GraphQLClient(url, {
+    headers: {
+      "Authorization" : process.env.GRAPH_CMS_TOKEN
+    }
+  })
+  const QUERY = gql`
+  {
+    properties {
+      street_address,
+      city,
+      state,
+      zip_code
+    }
+  }
+  `
+const data = await Client.request(QUERY)
+const properties = data.properties
+
+return{
+  props: {
+    properties
+  }
+}
+}
+
+
+
+
+export default function Home({properties}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +68,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>Owned Properties</h1>
+        {properties.map((property) => (
+          <h1>{property.street_address}, {property.city}</h1>
+        ))}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
