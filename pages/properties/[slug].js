8e15c63dde4b8fcd030336c6159a38a4d1373319
Chris Parker <cwbparker@gmail.com>
2022-07-02 13:51:17 -0700

    Dynamic routing for properties

---
+++ pages/properties/[slug].js
@@ -0,0 +1,43 @@
+import { GraphQLClient, gql } from 'graphql-request' 
+
+export const getServerSideProps = async (pageContext) => {
+    
+    const url = process.env.GRAPH_CMS_ENDPOINT
+    const client = new GraphQLClient(url, {
+      headers: {
+        "Authorization" : process.env.GRAPH_CMS_TOKEN
+      }
+    })
+
+    const pageSlug = pageContext.query.slug
+
+    const QUERY = gql`
+    query($pageSlug: String!) {
+        property(where: {slug: $pageSlug}) {
+          street_address,
+          city
+        }
+      }
+    `
+
+    const variables = {
+        pageSlug,
+    }
+
+    const data = await client.request(QUERY, variables)
+    const property = data.property
+
+    return {
+        props: {
+            property
+        }
+    }
+}
+
+const Property = ({ property }) => {
+    return (
+        <div>{property.street_address}</div>
+    )
+}
+
+export default Property
