0544d2333ac09d4d86e0ae1b5bbf88bddb219046
Chris Parker <cwbparker@gmail.com>
2022-07-21 07:19:32 -0700

    Add linking to nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,26 +1,43 @@
 import styles from '../styles/Nav.module.css'
+import Link from 'next/link'
 
 
 export default function Nav() {
     return (
         <div className={styles.container}>
             <div className={styles.innerContainer}>
-                <div className={styles.linkGroup}>
-                    <div>&&</div>
-                    <h3>Home</h3>
-                </div>
-                <div className={styles.linkGroup}>
-                    <div>&&</div>
-                    <h3>Properties</h3>
-                </div>
-                <div className={styles.linkGroup}>
-                    <div>&&</div>
-                    <h3>Maintenance</h3>
-                </div>
-                <div className={styles.linkGroup}>
-                    <div>&&</div>
-                    <h3>Expenses</h3>
-                </div>
+                <Link href="/">
+                    <a>
+                        <div className={styles.linkGroup}>
+                            <div>&&</div>
+                            <h3>Home</h3>
+                        </div>
+                    </a>
+                </Link>
+                <Link href="/properties">
+                    <a>
+                        <div className={styles.linkGroup}>
+                            <div>&&</div>
+                            <h3>Properties</h3>
+                        </div>
+                    </a>
+                </Link>
+                <Link href="/maintenance">
+                    <a>
+                        <div className={styles.linkGroup}>
+                            <div>&&</div>
+                            <h3>Maintenance</h3>
+                        </div>
+                    </a>
+                </Link>
+                <Link href="/expenses">
+                    <a>
+                        <div className={styles.linkGroup}>
+                            <div>&&</div>
+                            <h3>Expenses</h3>
+                        </div>
+                    </a>
+                </Link>
             </div>
         </div>
     )
