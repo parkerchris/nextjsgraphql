7561b42a3d71b163fd55ab99b8eeb229b39b9bb0
Chris Parker <cwbparker@gmail.com>
2022-07-23 09:14:19 -0700

    Create table in expenses page. Create expense form component.

---
+++ components/ExpenseForm.js
@@ -0,0 +1,28 @@
+export default function ExpenseForm() {
+    return (
+        <div>
+            <form>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+                <div>
+                    <label></label>
+                    <input></input>
+                </div>
+            </form>
+        </div>
+    )
+}
--- pages/expenses.js
+++ pages/expenses.js
@@ -1,65 +1,70 @@
 import Layout from '../components/Layout'
+import styles from '../styles/Expenses.module.css'
+import lemon from '../public/Revised_Lemon.png'
+import Image from 'next/image'
 
 export default function Expenses() {
     return (
         <Layout>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Property</th>
-                            <th>Category</th>
-                            <th>Description</th>
-                            <th>Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>11/15/12</td>
-                            <td>1405 Ridge Dr.</td>
-                            <td>Landscaping</td>
-                            <td>Paid Louis</td>
-                            <td>$115.00</td>
-                        </tr>
-                        <tr>
-                            <td>11/15/12</td>
-                            <td>1405 Ridge Dr.</td>
-                            <td>Landscaping</td>
-                            <td>Paid Louis</td>
-                            <td>$115.00</td>
-                        </tr>
-                        <tr>
-                            <td>11/15/12</td>
-                            <td>1405 Ridge Dr.</td>
-                            <td>Landscaping</td>
-                            <td>Paid Louis</td>
-                            <td>$115.00</td>
-                        </tr>
-                        <tr>
-                            <td>11/15/12</td>
-                            <td>1405 Ridge Dr.</td>
-                            <td>Landscaping</td>
-                            <td>Paid Louis</td>
-                            <td>$115.00</td>
-                        </tr>
-                        <tr>
-                            <td>11/15/12</td>
-                            <td>1405 Ridge Dr.</td>
-                            <td>Landscaping</td>
-                            <td>Paid Louis</td>
-                            <td>$115.00</td>
-                        </tr>
-                        <tr>
-                            <td>11/15/12</td>
-                            <td>1405 Ridge Dr.</td>
-                            <td>Landscaping</td>
-                            <td>Paid Louis</td>
-                            <td>$115.00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+           <div className={styles.mainContainer}>
+            <div className={styles.tableContainer}>
+                    <table className={styles.table}>
+                        <thead>
+                            <tr>
+                                <th className={styles.th}>Date</th>
+                                <th className={styles.th}>Property</th>
+                                <th className={styles.th}>Category</th>
+                                <th className={styles.th}>Description</th>
+                                <th className={styles.th}>Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className={styles.tr}>
+                                <td className={styles.thtd}>11/15/12</td>
+                                <td className={styles.thtd}>1405 Ridge Dr.</td>
+                                <td className={styles.thtd}>Landscaping</td>
+                                <td className={styles.thtd}>Paid Louis</td>
+                                <td className={styles.thtd}>$115.00</td>
+                            </tr>
+                            <tr className={styles.tr}>
+                                <td className={styles.thtd}>11/15/12</td>
+                                <td className={styles.thtd}>1405 Ridge Dr.</td>
+                                <td className={styles.thtd}>Landscaping</td>
+                                <td className={styles.thtd}>Paid Louis</td>
+                                <td className={styles.thtd}>$115.00</td>
+                            </tr>
+                            <tr className={styles.tr}>
+                                <td className={styles.thtd}>11/15/12</td>
+                                <td className={styles.thtd}>1405 Ridge Dr.</td>
+                                <td className={styles.thtd}>Landscaping</td>
+                                <td className={styles.thtd}>Paid Louis</td>
+                                <td className={styles.thtd}>$115.00</td>
+                            </tr>
+                            <tr className={styles.tr}>
+                                <td className={styles.thtd}>11/15/12</td>
+                                <td className={styles.thtd}>1405 Ridge Dr.</td>
+                                <td className={styles.thtd}>Landscaping</td>
+                                <td className={styles.thtd}>Paid Louis</td>
+                                <td className={styles.thtd}>$115.00</td>
+                            </tr>
+                            <tr className={styles.tr}>
+                                <td className={styles.thtd}>11/15/12</td>
+                                <td className={styles.thtd}>1405 Ridge Dr.</td>
+                                <td className={styles.thtd}>Landscaping</td>
+                                <td className={styles.thtd}>Paid Louis</td>
+                                <td className={styles.thtd}>$115.00</td>
+                            </tr>
+                            <tr className={styles.tr}>
+                                <td className={styles.thtd}>11/15/12</td>
+                                <td className={styles.thtd}>1405 Ridge Dr.</td>
+                                <td className={styles.thtd}>Landscaping</td>
+                                <td className={styles.thtd}>Paid Louis</td>
+                                <td className={styles.thtd}>$115.00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+           </div>
         </Layout>
     )
 }
